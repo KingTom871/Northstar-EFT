d05f810c1df67f37febede9032bea6ba93bfe3a3
EhsanMalik <em1130mail@gmail.com>
2026-06-08 00:26:51 -0700

    feat(layouts): 加入LayoutProvider来管理主页面布局状态，方便后期加入更多布局

---
+++ src/renderer/src/layouts/overlay/LayoutProvider.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+import { useAppStore } from "@/stores/index";
+import DefaultLayout from "@/layouts/overlay/presets/Default.vue";
+
+const appStore = useAppStore();
+
+const currentLayout = computed(() => {
+  const layoutName = appStore.layout;
+
+  switch (layoutName) {
+    case "default":
+      return DefaultLayout;
+    default:
+      return DefaultLayout;
+  }
+});
+</script>
+
+<template>
+  <component :is="currentLayout">
+    <slot />
+  </component>
+</template>
