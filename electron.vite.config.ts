694af30e1bf8e432a77214fe76c8b5657927cb67
EhsanMalik <em1130mail@gmail.com>
2026-06-07 23:59:30 -0700

    chore(dev-deps): 移除了自动引入

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,9 +1,6 @@
 import { resolve } from "path";
 import { defineConfig } from "electron-vite";
 import vue from "@vitejs/plugin-vue";
-import AutoImport from "unplugin-auto-import/vite";
-import Components from "unplugin-vue-components/vite";
-import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
 
 export default defineConfig({
   main: {},
@@ -14,19 +11,6 @@ export default defineConfig({
         "@": resolve("src/renderer/src"),
       },
     },
-    plugins: [
-      vue(),
-      AutoImport({
-        imports: ["vue", "vue-router"],
-        dts: "imports/auto-imports.d.ts",
-      }),
-      Components({
-        dirs: ["src/renderer/components"],
-        extensions: ["vue"],
-        resolvers: [NaiveUiResolver()],
-        deep: true,
-        dts: "imports/components.d.ts",
-      }),
-    ],
+    plugins: [vue()],
   },
 });
